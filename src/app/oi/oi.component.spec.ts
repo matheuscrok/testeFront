c69fcb953d08b9e8d2e1f31bf28ea79672afc1f5
crok <zenorhan@hotmail.com>
2021-03-06 15:57:37 -0300

    teste front git

---
+++ src/app/oi/oi.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { OiComponent } from './oi.component';
+
+describe('OiComponent', () => {
+  let component: OiComponent;
+  let fixture: ComponentFixture<OiComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ OiComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(OiComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
